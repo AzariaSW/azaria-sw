0c1b9afb9800ca208141a7c2adde63f325f037a4
AzariaSW <azariaandabenet@gmail.com>
2026-08-28 16:53:38 +0300

    chore(server): increase cache time for github requests

--- apps/server/src/config/github.config.js
+++ apps/server/src/config/github.config.js
@@ -8,11 +8,7 @@ const GITHUB = Object.freeze({
   TIMEOUT: 10000,
 
   CACHE: {
-    TTL: 15 * 60 * 1000,
-
-    PROJECTS: 5 * 60 * 1000,
-
-    PROFILE: 60 * 60 * 1000,
+    TTL: 3 * 60 * 60 * 1000,
 
     KEYS: {
       ACTIVITY: "github:activity",
