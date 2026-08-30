e88a848810fc3a6c702d997ccf9d1df5ec544f68
AzariaSW <azariaandabenet@gmail.com>
2026-08-30 21:38:42 +0300

    fix(client): fix request retry time

--- apps/client/src/lib/queryClient.js
+++ apps/client/src/lib/queryClient.js
@@ -4,6 +4,7 @@ export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       retry: 1,
+      retryDelay: 1500,
       refetchOnWindowFocus: false,
       staleTime: 1000 * 60 * 5,
       gcTime: 1000 * 60 * 10,
